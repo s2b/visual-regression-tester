82349a23a7bbdf60eb883ceb6bb1c5222607c674
Simon Praetorius <moin@praetorius.me>
2026-05-31 20:21:44 +0200

    [TASK] Setup build

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import UrlCollector from "./UrlCollector.js";
+import { getReportFromEnv, testsToRun } from "./Utils.js";
+import { VisualRegressionPage } from "./VisualRegressionPage.js";
+import VisualRegressionReporter from "./VisualRegressionReporter.js";
+
+export { UrlCollector, VisualRegressionPage, VisualRegressionReporter, getReportFromEnv, testsToRun };
