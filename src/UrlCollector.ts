d2c0f2c95c0b9de77ee33492b574f38026cd11f6
Simon Praetorius <moin@praetorius.me>
2026-06-03 21:25:20 +0200

    [TASK] Improve sitemap crawling output

--- src/UrlCollector.ts
+++ src/UrlCollector.ts
@@ -14,14 +14,14 @@ export default async (playwrightConfig: FullConfig) => {
   const report = getReport();
 
   if (!report.tests.length) {
-    console.log("crawling sitemaps...");
-    // TODO crawl robots.txt
+    console.log("Crawling sitemaps...");
     const sitemapper = new Sitemapper();
     const sitemaps = await Promise.all(
       config.sitemapUrls.map((sitemapUrl) => sitemapper.fetch(sitemapUrl)),
     );
     const testsAdded = new Set();
     sitemaps.forEach((sitemap) => {
+      console.log(`  ${sitemap.url} (${sitemap.sites.length} urls)`);
       sitemap.sites.forEach((url) => {
         const identifier = calculateIdentifier(url);
         if (testsAdded.has(identifier)) {
